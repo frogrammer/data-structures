612146ff9e0fa3ee5b1b34b24800f23a55898e5f
Tony Song <tony99019@gmail.com>
2016-10-15 19:19:08 -0500

    Add documentation to 'Heap.java'

--- queue/Heap.java
+++ queue/Heap.java
@@ -14,10 +14,18 @@ import java.util.Locale.LanguageRange;
 public class Heap<E extends Comparable<E>> {
 	private ArrayList<E> elements;
 	
+	/**
+	 * Constructs an empty heap.
+	 */
 	public Heap() {
 		elements = new ArrayList<E>();
 	}
 	
+	/**
+	 * Constructs a heap containing the elements of the specified 
+	 * list.
+	 * @param initial the list whose elements are to be placed into this heap
+	 */
 	public Heap(List<E> initial) {
 		ArrayList<E> tempArr = new ArrayList<E>(initial);
 
@@ -26,28 +34,57 @@ public class Heap<E extends Comparable<E>> {
 		}
 		elements = tempArr;
 	}
-
+	
+	/**
+	 * Finds the index of the parent node of the given child node.
+	 * It returns 0 if 0 is given.
+	 * @param child the index of a child node
+	 * @return the parent index of the given child index
+	 */
 	private int findParent(int child) {
 		return (child - 1) / 2;
 	}
 	
+	/**
+	 * Finds the index of the left child node of the given parent node.
+	 * It does not check if the left child exists.
+	 * @param parent the index of a parent node
+	 * @return the (expected) left child index of the given parent index
+	 */
 	private int findLeftChild(int parent) {
 		return parent * 2 + 1;
 	}
 	
+	/**
+	 * Finds the index of the right child node of the given parent node.
+	 * It does not check if the right child exists.
+	 * @param parent the index of a parent node.
+	 * @return the (expected) right child index of the given parent index.
+	 */
 	private int findRightChild(int parent) {
 		return parent * 2 + 2;
 	}
 	
+	/**
+	 * Returns the number of elements in this heap.
+	 * @return the number of elements in this heap.
+	 */
 	public int size() {
 		return elements.size();
 	}
 	
+	/**
+	 * Returns true if this heap contains no elements.
+	 * @return true if this heap contains no elements.
+	 */
 	public boolean isEmpty() {
 		return elements.isEmpty();
 	}
 	
-	// Assume everything below index assumed to be heaps and arr is not empty
+	/**
+	 * Make a heap tree whose root is at index. The root may violate the heap property,
+	 * but both of its subtrees are heaps. Assume the given ArrayList is not empty.
+	 */
 	private void heapify(ArrayList<E> arr, int index) {
 		int left = findLeftChild(index);
 		int right = findRightChild(index);
@@ -72,6 +109,10 @@ public class Heap<E extends Comparable<E>> {
 		}
 	}
 	
+	/**
+	 * Inserts the specified element into this heap.
+	 * @param element the element to add
+	 */
 	public void add(E element) {
 		if (element == null) throw new NullPointerException();
 		
@@ -92,12 +133,20 @@ public class Heap<E extends Comparable<E>> {
 		}
 	}
 	
+	/**
+	 * Retrieves, but does not remove, the head of this heap, or returns null if this heap is empty.
+	 * @return the head of this heap, or null if this heap is empty
+	 */
 	public E peek() {
 		if (elements.isEmpty()) return null;
 		
 		return elements.get(0);
 	}
 	
+	/**
+	 * Retrieves and removes the head of this heap, or returns null if this heap is empty.
+	 * @return the head of this heap, or null if this heap is empty
+	 */
 	public E remove() {
 		if (elements.isEmpty()) return null;
 
